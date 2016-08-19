db9bad673f8863e5f121134c081513ebe1da73f0
Drashti <86737.parikh@students.itu.edu>
2016-08-19 12:52:22 -0700

    Added else If condition

--- TestClass.cs
+++ TestClass.cs
@@ -6,7 +6,9 @@ public class TheMotivator
     {
         if (score == 100)
             Console.WriteLine("You're awesome");
-		else
+        else if (score > 90)
+            Console.WriteLine("That's great");
+        else
             Console.WriteLine("Well, what can I say?");
     }
     public static void main(String[] args)
